542b7bda86dccf5632b3cd48fea65eb18343b620
Wulandary14 <Moonywulan14@gmail.com>
2026-06-09 11:17:45 +0700

    void postorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -107,4 +107,19 @@ public:
         }
     }
 
-    
+    void postorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
